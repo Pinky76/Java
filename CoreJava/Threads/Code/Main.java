fb60a139c0bbb538148e4ea8db41358a376c79af
ManasaM8 <manasamanthena8@gmail.com>
2023-06-14 10:06:22 -0500

    Threads Commit June 14

---
+++ CoreJava/Threads/Code/Main.java
@@ -0,0 +1,16 @@
+
+public class Main extends Thread{
+    public void run(){
+        System.out.println("Implementing Threads extending Thread class :: inside run method:");
+        for(int i = 0 ; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+    }
+    public static void main(String[] args) {
+    Main thread = new Main();
+    thread.start();
+    ThreadImplementation thread2 = new ThreadImplementation();
+    thread2.run();
+    }
+}
---
+++ CoreJava/Threads/Code/ThreadImplementation.java
@@ -0,0 +1,11 @@
+public class ThreadImplementation implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Implementing threads using Runnable Method :: Inside run method");
+        for(int i =0; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+}
